75cf96d99c4df7f49c7647eba34f255493f97505
WilcoApp <hello@wilco.gg>
2023-06-08 01:14:10 +0000

    Search party

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,6 @@
 WILCO_ID="`cat .wilco`"
 ENGINE_EVENT_ENDPOINT="${ENGINE_BASE_URL}/users/${WILCO_ID}/event"
-CODESPACE_BACKEND_HOST="${CODESPACE_NAME}-3000.preview.app.github.dev"
+CODESPACE_BACKEND_HOST="${CODESPACE_NAME}-3000.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
 CODESPACE_BACKEND_URL="https://${CODESPACE_BACKEND_HOST}"
 
 # Update engine that codespace started for user
@@ -17,7 +17,7 @@ echo "printf \"\n=============================================\n\"" >> ~/.bashrc
 echo "gh codespace ports -c $CODESPACE_NAME" >> ~/.bashrc
 echo "printf \"=============================================\n\"" >> ~/.bashrc
 echo "printf \"(Once docker-compose is up and running, you can access the frontend and backend using the above urls)\n\"" >> ~/.bashrc
-echo "printf \"\n👉 Type: \\\`docker-compose up\\\` to run the project. 👈\n\n\"" >> ~/.bashrc
+echo "printf \"\n\x1b[31m \x1b[1m👉 Type: \\\`docker-compose up\\\` to run the project. 👈\n\n\"" >> ~/.bashrc
 
 # Change backend port visibility to public
 echo "(&>/dev/null .devcontainer/open_port.sh &)" >> ~/.bashrc
